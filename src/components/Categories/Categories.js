def4de31eaf6bf8a5309e15ea2af2e1dae68d906
Mahmoud El Helou <mahelhelou@gmail.com>
2022-05-01 18:19:36 +0300

    Updating README file

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,22 +1,24 @@
-import React, { useEffect, useContext } from "react";
-import { myContext } from "../context/context";
-import './Categories.scss';
+import React, { useEffect, useContext } from 'react'
+import { myContext } from '../context/context'
+import './Categories.scss'
+
 const Categories = () => {
-  const { fetchCategories, categories } = useContext(myContext);
+	const { fetchCategories, categories } = useContext(myContext)
 
-  useEffect(() => {
-    fetchCategories();
-  }, [fetchCategories]);
-  return (
-    <div className="categories">
-      {categories.map((category) => (
-        <div key={category.idCategory}>
-          <img src={category.strCategoryThumb} alt="#" />
-          <h4>{category.strCategory}</h4>
-        </div>
-      ))}
-    </div>
-  );
-};
+	useEffect(() => {
+		fetchCategories()
+	}, [fetchCategories])
 
-export default Categories;
+	return (
+		<div className='categories'>
+			{categories.map(category => (
+				<div key={category.idCategory}>
+					<img src={category.strCategoryThumb} alt='#' />
+					<h4>{category.strCategory}</h4>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default Categories
